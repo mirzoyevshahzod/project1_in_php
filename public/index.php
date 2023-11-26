8299dd6c4d0e8369116852077146b2dd7459f5b3
mirzoyevshahzod <131646088+mirzoyevshahzod@users.noreply.github.com>
2023-11-26 17:18:14 +0500

    vendor
    vendor added

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
-$app = include __DIR__ ."/../src/App/bootstrap.php";
+echo "<pre>";
+print_r($_SERVER);
+echo "<pre>";
+
+$app = include __DIR__ . "/../src/App/bootstrap.php";
 
 $app -> run();
+
+
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,9 +2,18 @@
 
 declare(strict_types= 1);
 
-require __DIR__ ."/../../vendor/autoload.php";
+require __DIR__ . "/../../vendor/autoload.php";
+
+
 
 use Framework\App;
+use App\Controllers\HomeController;
+
+
 $app = new App();
+
+$app->get('/', [HomeController::class, 'home']);
+
+
 
 return $app;
